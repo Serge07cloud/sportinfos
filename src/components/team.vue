ccca83b936dd5ac0e6a026b140850cf7fdeaabb7
wisdom-art <wisdomandy.5@gmail.com>
2021-01-27 03:09:17 +0000

    Beginning

---
+++ src/components/team.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card team">
+    <h1 class="card-header">Hello world !</h1>
+  </div>
+</template>
+
+<script>
+export default {
+name: "team"
+}
+</script>
+
+<style scoped>
+
+</style>
